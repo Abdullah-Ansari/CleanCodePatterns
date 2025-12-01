9810ded98188765201f591eccd2437aebf821f0c
Abdullah Ansari <abdullahansari018@gmail.com>
2025-12-01 17:57:15 +0530

    Initial Commit

---
+++ Swift-Design-Patterns-and-SOLID/Swift-Design-Patterns-and-SOLID/Swift_Design_Patterns_and_SOLIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Design_Patterns_and_SOLIDApp.swift
+//  Swift-Design-Patterns-and-SOLID
+//
+//  Created by Abdullah Ansari on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Design_Patterns_and_SOLIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
